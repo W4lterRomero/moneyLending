0268159dd337bd57e2bc088eee7d1b1ac68b1e20
W4lterRomero <rh23015@ues.edu.sv>
2025-12-10 23:37:11 -0600

    fix: íconos del modo día modo noche

--- proyectos/moneyLanding/resources/views/components/icon.blade.php
+++ proyectos/moneyLanding/resources/views/components/icon.blade.php
@@ -23,7 +23,7 @@
     $path = $icons[$name] ?? $icons['dots'];
 @endphp
 
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
     {{ $attributes->merge(['class' => $class]) }}>
     {!! $path !!}
 </svg>
